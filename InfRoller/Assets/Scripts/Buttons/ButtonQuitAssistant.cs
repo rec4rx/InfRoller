2754087d944435d03570f3217d1711eb3ddf16ff
Dat Tran <rec4rx@gmail.com>
2019-01-19 22:59:01 +0700

    compiling test

---
+++ InfRoller/Assets/Scripts/Buttons/ButtonQuitAssistant.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonQuitAssistant : Button
+{
+// Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        Debug.Log("OnClick Quit!");
+    }
+}
